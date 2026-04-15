40b7abc99d76748c18482b87522658f2003219ca
JohnAtGmail <kossjohn@gmail.com>
2026-04-15 14:10:39 -0400

    Saving

--- variables.tf
+++ variables.tf
@@ -2,12 +2,12 @@ variable "lambda" {
   type = object({
     name             = string
     path             = string
+    github_token     = string
     arch             = optional(string, "arm64")
     description      = optional(string)
     timeout          = optional(number, 5)
     memory_size      = optional(number, 128)
     other_args       = optional(string, "")
-    docker_host      = optional(string, "tcp://127.0.0.1:2375")
     managed_policies = optional(list(string))
     inline_policies = optional(list(object({
       name   = string
